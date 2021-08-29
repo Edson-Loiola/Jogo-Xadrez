9a66001d18e9fc0823b8630e44273fa02443842f
Edson Monteiro Loiola <edson.monteiro.l@gmail.com>
2021-08-29 13:42:04 -0300

    Tratando erros com excecao perosnalizada

--- tabuleiro/Tabuleiro.cs
+++ tabuleiro/Tabuleiro.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using tabuleiro;
 
 namespace tabuleiro
 {
@@ -23,13 +24,51 @@ namespace tabuleiro
             return pecas[linha, coluna];
         }
 
+        //sobrecarga do metodo peca
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+
+        //verificar se tem peça em uma dada posição
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
+
+
         //colocar uma peça no tabuleiro
         public void colocarPeca(Peca p, Posicao pos)
         {
+
+            // validar se já nem uma peça na mesma posição
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Já existe uma peça nessa posição!");
+            }
             //matriz de peças
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
         }
 
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        //lançar exception personalizada
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
+        }
     }
 }
