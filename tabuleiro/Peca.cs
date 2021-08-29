c6be640abd945ea7eddf4c916d8291735ef7a82e
Edson Monteiro Loiola <edson.monteiro.l@gmail.com>
2021-08-28 21:49:26 -0300

    Implementando o tabuleiro

---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; } //pode ser acessada por outras classes mas só pode ser alterada ela mesmo sub classes
+        public int qteMovimentos { get; protected set; } //pode ser acessada por outras classes mas só pode ser alterada ela mesmo sub classes
+        public Tabuleiro tab { get; protected set; }
+
+        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        {
+            this.posicao = posicao;
+            this.tab = tab;
+            this.cor = cor;
+            this.qteMovimentos = 0;
+        }
+
+    }
+}
