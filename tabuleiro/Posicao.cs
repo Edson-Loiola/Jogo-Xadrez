dca3aa33200ad1bbbe5504a9ff370e9399c9e8ae
Edson Monteiro Loiola <edson.monteiro.l@gmail.com>
2021-08-28 21:21:57 -0300

    Crinado a classe posicao

---
+++ tabuleiro/Posicao.cs
@@ -0,0 +1,24 @@
+﻿namespace tabuleiro
+{
+    class Posicao
+    {
+        public int linha { get; set; }  //get set (incapsulamento) podem ser alterados por outras classes
+        public int coluna { get; set; }
+
+        //construtor 
+        public Posicao(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        //override para imprimir o objeto inteiro
+        public override string ToString()
+        {
+            return linha
+                + ", "
+                + coluna;
+        }
+
+    }
+}
